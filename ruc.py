34f2a0cb0d6b9d698058129c77082e9dd098cec4
Austen Lamacraft <austen@lamacraft.io>
2019-01-14 08:17:14 +0000

    New notebooks

--- ruc.py
+++ ruc.py
@@ -1,83 +1,137 @@
+import math
 import numpy as np
-from numba import jit, complex128
 from scipy.stats import unitary_group
+from scipy.linalg import eigh
 
-def ruc_channel(ρ):
+
+def cptp_map(ρ, gates):
     """
-    Quantum channel corresponding to fixed depth random
-    unitary circuit.
+    Quantum channel corresponding to fixed depth circuit defined by gates.
 
     ρ is a numpy array of shape `[q,q,...q]` (`2*depth` times),
     where q is the local Hilbert space dimension.
 
-    We assume `depth` row indices followed by `depth` column indices.
+    We assume `depth` row alternating with `depth` column indices: [r0, c0, r1, c1, ...]
 
     Returns: output density matrix of the same shape
     """
 
-    shape = ρ.shape
-    depth = len(shape) // 2
-    q = shape[0]
-    gates = random_gates(q, depth)
-    conj_gates = [contig_conj_transpose(gate) for gate in gates]
-    conj_gates.reverse()
+    # Trace out the first index
+    ρ = np.trace(ρ, axis1=0, axis2=1)
 
-    # First trace out the final index
-    ρ = np.trace(ρ, axis1=depth-1, axis2=2*depth-1)
+    # We are going to 'a,h,...' for 'in' indices, `x,y,.... for 'out'
+    # Capitals for contractions between unitaries
+    # After contraction we move indices to the end using ellipsis notation
+    # After going through all the gates the indices are back in their starting position.
 
-    # Add two new indices of size 1
-    ρ = np.expand_dims(ρ, axis=depth-1)
-    ρ = np.expand_dims(ρ, axis=0)
+    ρ = np.einsum('aACx,bBCy,ab...->AB...xy', gates[0], gates[0].conj(), ρ,
+                  optimize=['einsum_path', (0, 1), (0, 1)])
 
-    # We have to pass the shape tuple and gates into the decorated functions
-    ρ_out = mat_mul(ρ, shape, gates, conj_gates)
+    for gate in gates[1:-1]:
+        ρ = np.einsum('aACx,bBDy,CDab...->AB...xy', gate, gate.conj(), ρ,
+                      optimize=['einsum_path', (0, 2), (0, 1)])
 
-    return ρ_out
+    ρ = np.einsum('Cx,Dy,CD...->...xy', gates[-1][0, 0], gates[-1][0, 0].conj(), ρ,
+                  optimize=['einsum_path', (0, 2), (0, 1)])
 
-# @jit(nopython=True)
-def mat_mul(ρ, out_shape, gates, conj_gates):
-
-    ρ_out = np.zeros(out_shape, dtype=np.complex128)
-
-    for out_idx in np.ndindex(*out_shape):
-        ρ_out_elem = 0
-        for in_idx, ρ_elem in np.ndenumerate(ρ):
-            res = matrix_product(out_idx, in_idx, gates, conj_gates)
-            res *= ρ_elem
-            ρ_out_elem += res
-
-        ρ_out[out_idx] = ρ_out_elem
-
-    return ρ_out
+    return ρ
 
 
-# @jit(nopython=True)#, locals={'gate_matrix': complex128[:,::1]})
-def matrix_product(out_idx, in_idx, gates, conj_gates):
+def apply_gates(state, gates):
+    """
+    Apply unitary gates to ancilla states, starting with a randomly chosen pair of final states.
+    Resulting state is then normalized.
+    """
 
-    q = gates[0].shape[0]
-    depth = len(gates)
+    q = state.shape[0]
+    traj = np.random.randint(q, size=2)
+    state = np.einsum('aA,a...->A...', gates[0][:, :, traj[0], traj[1]], state)
 
-    left_vec = np.zeros(q, dtype=np.complex128)
-    left_vec[0] = 1
+    for gate in gates[1:-1]:
+        state = np.einsum('aABx,Ba...->A...x', gate, state)
 
-    for idx, gate in enumerate(gates):
-        # Doing this without slices removed a Numba performance warning
-        gate_matrix = gate[in_idx[idx], out_idx[idx]]
-        left_vec = np.dot(left_vec, gate_matrix)
+    state = np.einsum('Bx,B...->...x', gates[-1][0, 0], state)
 
-    for idx, gate in enumerate(conj_gates, 1):
-        # Annoyingly, Numba doesn't seem to like negative indices, so we do this.
-        gate_matrix = gate[in_idx[2 * depth - idx], out_idx[2 * depth - idx]]
-        left_vec = np.dot(left_vec, gate_matrix)
+    return state / np.sqrt(inner_product(state, state))
 
-    right_vec = np.zeros(q, dtype=np.complex128)
-    right_vec[0] = 1
-
-    res = np.dot(left_vec, right_vec)
-    return res
 
 def random_gates(q, depth):
-    return [np.ascontiguousarray(unitary_group.rvs(q ** 2).reshape(4 * [q]).transpose(1, 2, 0, 3)) for _ in range(depth)]
+    return unitary_group.rvs(q ** 2, size=depth).reshape([depth, q, q, q, q])
 
-def contig_conj_transpose(array):
-    return np.ascontiguousarray(array.conj().transpose(0, 1, 3, 2))
+
+def tensor_trace(tensor):
+    """
+    Calculate the matrix trace of a tensor with rows and column multi-indices as [r0, c0, r1, c1, ...]
+    """
+    while len(tensor.shape) != 0:
+        tensor = np.einsum("aa...->...", tensor)
+
+    return tensor
+
+
+def tensor_conj_transp(tensor):
+    """
+    Calculate the conjugate transpose of a tensor with rows and column multi-indices as [r0, c0, r1, c1, ...]
+    """
+    tensor = tensor.conj()
+    num_indices = len(tensor.shape)
+    trans_perm = np.arange(num_indices).reshape([-1,2])[:,::-1].reshape([-1])
+    tensor = tensor.transpose(trans_perm)
+
+    return tensor
+
+
+def trace_square(tensor):
+    """
+    Calculate the trace of the square of a tensor with rows and column multi-indices as [r0, c0, r1, c1, ...]
+    """
+    indices1 = "abcdefghijklmnopqrstuvwxyz"
+    indices2 = "badcfehgjilknmporqtsvuxwzy"
+    num_indices = len(tensor.shape)
+    einsum_str = indices1[:num_indices] + "," + indices2[:num_indices] + "->"
+    return np.einsum(einsum_str, tensor, tensor)
+
+
+def inner_product(state1, state2):
+    """
+    Calculate the inner product of two states.
+    """
+    assert(state1.shape == state2.shape)
+    num_indices = len(state1.shape)
+    return np.tensordot(state1.conj(), state2, axes=num_indices)
+
+
+def tensor_to_matrix(tensor):
+    depth = len(tensor.shape) // 2
+    q = tensor.shape[0]
+    tensor = tensor.transpose(list(range(0, 2 * depth, 2)) + list(range(1, 2 * depth, 2)))
+    return tensor.reshape(2 * [q ** depth])
+
+
+def matrix_to_tensor(matrix, q):
+    depth = int(math.log(matrix.shape[0], q))
+    tensor = matrix.reshape(2 * depth * [q])
+    axis_order = np.arange(2 * depth).reshape([2, -1]).T.reshape([-1])
+    return tensor.transpose(axis_order)
+
+
+def random_ρ(q, depth):
+    """
+    Generate a random density matrix with row and column multi-indices as [r0, c0, r1, c1, ...]
+    """
+    random_matrix = np.random.rand(*2*[q**depth])
+    random_matrix = random_matrix @ random_matrix.transpose()
+    random_tensor = matrix_to_tensor(random_matrix, q)
+    ρ = random_tensor / tensor_trace(random_tensor)
+    return ρ
+
+
+def random_state(q, depth):
+    """
+    Generate a random normalized state
+    """
+    random_state = np.random.rand(* depth * [q])
+    return random_state / np.sqrt(inner_product(random_state, random_state))
+
+
+
--- test_ruc.py
+++ test_ruc.py
@@ -1,50 +1,81 @@
 import unittest
 import numpy as np
 from numpy.testing import assert_allclose, assert_almost_equal
-from ruc import random_gates, ruc_channel
+from ruc import *
 
 
 class TestRandomCircuitFunctions(unittest.TestCase):
 
     def testRandomGatesAreUnitary(self):
         q = 2
-        depth = 3
+        depth = 4
         gates = random_gates(q, depth)
-        gates_as_matrices = [gate.transpose(2,0,1,3).reshape(2*[q**2]) for gate in gates]
+        gates_as_matrices = [gate.reshape(2*[q**2]) for gate in gates]
         uudag = [gate @ gate.conj().transpose() for gate in gates_as_matrices]
         [assert_allclose(prod, np.identity(q**2, dtype=complex), atol=1e-14) for prod in uudag]
 
     def testGatesAreCContiguous(self):
         q = 2
-        depth = 3
+        depth = 4
         gates = random_gates(q, depth)
         for gate in gates:
             assert(gate.flags['C_CONTIGUOUS'] == True)
 
-    def testRucChannelTracePreserving(self):
+    def testIdentityGatesRepeatedGivesPureState(self):
         q = 2
-        depth = 3
-        random_matrix = np.random.rand(*2*[q**depth])
-        random_matrix = (random_matrix + random_matrix.transpose()) / 2
-        input_trace = np.trace(random_matrix)
-        input_tensor = random_matrix.reshape(2*depth*[q])
-        output_tensor = ruc_channel(input_tensor)
-        output_trace = np.trace(output_tensor.reshape(2*[q**depth]))
+        depth = 4
+        id_gates = [np.identity(q ** 2).reshape([q, q, q, q]) for _ in range(depth)]
+        rho = random_ρ(q, depth)
+        for _ in range(2):
+            rho = cptp_map(rho, id_gates)
+
+        output_evals = eigh(tensor_to_matrix(rho), eigvals_only=True)
+        expected = np.zeros(q**depth)
+        expected[-1] = 1.
+        assert_almost_equal(expected, output_evals)
+
+    def testCPTPMapTracePreserving(self):
+        q = 2
+        depth = 4
+        input_rho = random_ρ(q, depth)
+        input_trace = tensor_trace(input_rho)
+        output_rho = cptp_map(input_rho, random_gates(q, depth))
+        output_trace = tensor_trace(output_rho)
         assert_almost_equal(input_trace, output_trace)
 
-    def testRucChannelHermiticityPreserving(self):
+    def testCPTPMapHermiticityPreserving(self):
         q = 2
         depth = 3
-        random_matrix = np.random.rand(*2 * [q ** depth])
-        random_matrix = (random_matrix + random_matrix.transpose()) / 2
-        input_tensor = random_matrix.reshape(2 * depth * [q])
-        output_tensor = ruc_channel(input_tensor)
-        output_matrix = output_tensor.reshape(2 * [q ** depth])
-        assert_almost_equal(output_matrix, output_matrix.conj().transpose())
+        input_rho = random_ρ(q, depth)
+        input_rho = (input_rho + tensor_conj_transp(input_rho)) / 2
+        output_rho = cptp_map(input_rho, random_gates(q, depth))
+        assert_almost_equal(output_rho, tensor_conj_transp(output_rho))
 
-    def testRucChannelPositive(self):
+    def testCPTPMapPositive(self):
         """
         Use Choi form to test for positivity
         """
         pass
 
+    def testRandomRhoUnitTrace(self):
+        q = 2
+        depth = 3
+        rho = random_ρ(q, depth)
+        trace = tensor_trace(rho)
+        assert_almost_equal(trace, 1.)
+
+    def testApplyGatesPreservesNorm(self):
+        q = 2
+        depth = 4
+        gates = random_gates(q, depth)
+        state = random_state(q, depth)
+        state = apply_gates(state, gates)
+        assert_almost_equal(1., inner_product(state, state))
+
+    def testInnerProductsLessThanUnity(self):
+        q = 2
+        depth = 4
+        for _ in range(10):
+            state1 = random_state(q, depth)
+            state2 = random_state(q, depth)
+            assert(np.abs(inner_product(state1, state2)) < 1.)
